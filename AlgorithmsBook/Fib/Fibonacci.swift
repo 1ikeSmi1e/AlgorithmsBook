9f434aae5f3c87ec644e7080dafebe3bda0c9ab8
Dandy_zhouli <zhouliqiang@jmfen.cn>
2021-01-22 16:16:50 +0800

    斐波那契算法添加时间复杂度

--- AlgorithmsBook/Fib/Fibonacci.swift
+++ AlgorithmsBook/Fib/Fibonacci.swift
@@ -9,6 +9,7 @@ import UIKit
 
 ///斐波那契数列数列算法: 0, 1, 2, 3, 5, 13...
 class Fibonacci: NSObject {
+    /// 时间复杂度：O(2^n)
     /// 第一种方式：递归--》效率低的算法：在month很大的时候需要计算时间很长64
     /// - Parameter month: 第month月份
     /// - Returns: 第month月会有的斐波那契
@@ -19,6 +20,8 @@ class Fibonacci: NSObject {
         return recursionCal(month - 1) + recursionCal(month - 2)
     }
     
+    
+    /// 第二种方式：使用for循环进行计算，时间复杂度：O(n)
     static func forCal(_ month: Int) -> NSInteger{
         if month < 2 {
             return month
